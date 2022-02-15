2d123922439f849489766d28ed0f504ca004d1bb
bobubear <jedsa.pd@gmail.com>
2022-02-15 18:14:40 +0700

    All Complete

--- lab25.cpp
+++ lab25.cpp
@@ -24,10 +24,70 @@ ComplexNumber ComplexNumber::operator+(const ComplexNumber &c){
 	return ComplexNumber(real+c.real,imag+c.imag);
 }
 
+ComplexNumber operator+(double s,const ComplexNumber &c){
+	return ComplexNumber(s+c.real,c.imag);
+}
+
 ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 	return ComplexNumber(real-c.real,imag-c.imag);
 }
 
+ComplexNumber operator-(double s,const ComplexNumber &c){
+	return ComplexNumber(s-c.real,-c.imag);
+}
+
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber(real*c.real-imag*c.imag,imag*c.real+real*c.imag);
+}
+
+ComplexNumber operator*(double s,const ComplexNumber &c){
+	return ComplexNumber(s*c.real-0*c.imag,0*c.real+s*c.imag);
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	return ComplexNumber((real*c.real+imag*c.imag)/(pow(c.real,2)+pow(c.imag,2)),(imag*c.real-real*c.imag)/(pow(c.real,2)+pow(c.imag,2)));
+}
+
+ComplexNumber operator/(double s,const ComplexNumber &c){
+	return ComplexNumber((s*c.real+0*c.imag)/(pow(c.real,2)+pow(c.imag,2)),(0*c.real-s*c.imag)/(pow(c.real,2)+pow(c.imag,2)));
+}
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	if(real == c.real && imag == c.imag)
+		return true;
+	else
+		return false;
+}
+
+bool operator==(double s,const ComplexNumber &c){
+	if(s == c.real && 0 == c.imag)
+		return true;
+	else
+		return false;
+}
+
+double ComplexNumber::abs(){
+	return sqrt(pow(real,2)+pow(imag,2));
+}
+
+double ComplexNumber::angle(){
+	return atan2(imag,real) * 180 / M_PI;
+}
+
+ostream & operator<<(ostream &os, const ComplexNumber &c){
+	if(c.real == 0 && c.imag == 0)
+		return os << 0;
+	else{
+		if(c.real == 0)
+			return os << c.imag << "i";
+		else if(c.imag == 0)
+			return os << c.real;
+		else if(c.imag > 0)
+			return os << c.real << "+" << c.imag << "i";
+		else
+			return os << c.real << c.imag << "i";
+	} 
+}
 //Write your code here
 
 int main(){
